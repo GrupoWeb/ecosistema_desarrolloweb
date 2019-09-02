cb3ca52006226e9455cc4af15634df35a2a0003d
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2019-09-02 16:33:43 -0600

    node

--- Estructura/node_umg/src/database.js
+++ Estructura/node_umg/src/database.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/votos',{
+// const MONGO_USERNAME = 'sammy';
+// const MONGO_PASSWORD = 'your_password';
+// const MONGO_HOSTNAME = '172.20.0.10';
+// const MONGO_PORT = '27017';
+// const MONGO_DB = 'sharkinfo';
+
+// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+// mongoose.connect(url, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useFindAndModify: false
+// })
+// .then(dv => console.log('DB is connect'))
+// .catch(err => console.log(err));
+// const connectDb = () => {
+return mongoose.connect('mongodb://127.0.0.1:27017/votos',{
+// return mongoose.connect('mongodb://172.20.0.10:8081//votos',{
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
 })
     .then(dv => console.log('DB is connect'))
     .catch(err => console.log(err));
-
+// }
+// module.exports = connectDb;
--- Estructura/node_umg/src/models/votos.js
+++ Estructura/node_umg/src/models/votos.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const VotosShema = new VotosShema({
-    
-})
+const VotosShema = new Schema({
+    name: { type: String, required: true },
+    character: { type: String, required: true },
+});
+
+module.exports = mongoose.model('VotosShema', VotosShema)
