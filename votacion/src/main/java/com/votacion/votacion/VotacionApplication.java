f7c56af0dc1f36cf9d43fbd984f0b8abea862dc5
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2019-09-13 10:47:57 -0600

    add

--- votacion/src/main/java/com/votacion/votacion/VotacionApplication.java
+++ votacion/src/main/java/com/votacion/votacion/VotacionApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication /*
-						 * (exclude = {SecurityAutoConfiguration.class,
-						 * UserDetailsServiceAutoConfiguration.class})
-						 */
+@SpringBootApplication/*(exclude = {SecurityAutoConfiguration.class,
+		UserDetailsServiceAutoConfiguration.class})*/
 @ComponentScan(basePackages = { "com.votacion.votacion" })
 public class VotacionApplication {
 
